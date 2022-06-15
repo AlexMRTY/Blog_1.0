4a5b79078cc0fa3cd9087a99e3a46a0f924d4026
AlexMRTY <asifrezai261@gmail.com>
2022-06-15 15:20:34 +0200

    Edited the port

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.post("/compose", (req, res) => {
   res.redirect("/")
 })
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 5000, function() {
+  console.log("Server started on port 5000");
 });
